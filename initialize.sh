660989d3331b850716649322e6c04a2a67c37f05
David Villasmil <david.villasmil.work@gmail.com>
2021-09-13 22:54:33 +0100

    Set systemd to start kamailio

--- initialize.sh
+++ initialize.sh
@@ -39,4 +39,17 @@ USER=kamailio
 GROUP=kamailio
 CFGFILE=/etc/kamailio/kamailio.cfg
 
-/usr/sbin/kamailio -P /var/run/kamailio/kamailio.pid -f $CFGFILE -m $SHM_MEMORY -M $PKG_MEMORY -u $USER -g $GROUP -DD -E
+echo '
+#
+# Kamailio startup options
+#
+
+RUN_KAMAILIO=yes
+SHM_MEMORY=64
+PKG_MEMORY=8
+USER=kamailio
+GROUP=kamailio
+CFGFILE=/etc/kamailio/kamailio.cfg
+' > /etc/default/kamailio
+service kamailio start
+#/usr/sbin/kamailio -P /var/run/kamailio/kamailio.pid -f $CFGFILE -m $SHM_MEMORY -M $PKG_MEMORY -u $USER -g $GROUP -DD -E
